e39b9ba2b55d1feb088301360fad6d7921358cb6
Tianchi Zhang <tianchi.zhang01@gmail.com>
2021-05-13 00:50:06 -0400

    Home page prev/next buttons styles

--- flask_app/static/js/home.js
+++ flask_app/static/js/home.js
@@ -1,11 +1,19 @@
-// d3.selectAll(".nav-link").on("click", function() {
-//     var self = d3.select(this);
+// Add custom responsive stylings to clickable carousel objects
 
-//     if (self.classed("nav-link")) {
-//         d3
-//             .append("active")
-//     }
-//     else {
+// Carousel "Previous" and "Next" buttons
+buttons = d3.selectAll("button");
 
-//     }
-// })
+buttons.on("mouseover", function() {
+    d3.select(this)
+    .style("opacity", 0.5)
+})
+.on("mouseout", function() {
+    d3.select(this)
+    .style("opacity", 0.2)
+    .style("background-color", "slateGray")
+})
+.on("click", function() {
+    d3.select(this)
+    .style("opacity", 0.6)
+    .style("background-color", "steelBlue")
+});
